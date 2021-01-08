38e8ea12d04c157462d691eeebdac8847e25e186
Magnus <beachmagnus+git@gmail.com>
2021-01-08 23:44:49 +0100

    basics

---
+++ src/components/game/game.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Board from "../board/board.component";
+
+const Game = () => (
+  <div className="game">
+    <div className="game-board">
+      <Board />
+    </div>
+  </div>
+);
+
+export default Game;
---
+++ src/components/square/square.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Square = () => (
+  <button className="square">
+    {/* {
+                    TODO
+                } */}
+  </button>
+);
+
+export default Square;
